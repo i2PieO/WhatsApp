45a666e27f3806555ae273a26e630c6db7dd8f58
Syed Khundmir Jawad <102740106+i2PieO@users.noreply.github.com>
2024-05-31 18:11:41 +0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,82 @@
 // importing 
 import express from 'express'
 import mongoose from 'mongoose'
+import Messages from './dbMessages.js';
+import Pusher from 'pusher';
+import cors from 'cors';
 
 // app config 
 const app = express()
 const port = process.env.PORT || 9000
 
+const pusher = new Pusher({
+    appId: "1808370",
+    key: "03be9f11202624763392",
+    secret: "9b1351f7b3314cfe0426",
+    cluster: "ap2",
+    useTLS: true
+  });
+
 // middleware
+app.use(express.json());
+app.use(cors())
 
 // DB config 
-const connection_url = 'mongodb+srv://admin:cwAD1qSjFqIIyOUb@cluster0.ffezfum.mongodb.net/whatsappdb?retryWrites=true&w=majority&appName=Cluster0'
+const connection_url = 'mongodb+srv://admin:cwAD1qSjFqIIyOUb@cluster0.ffezfum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0cwAD1qSjFqIIyOUb'
 
-mongoose.connect(connection_url,{
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(connection_url)
 
-// ????
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection('messagecontents')
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A Change occured", change);
+
+    if (change.operationType === 'insert') {
+        const messageDetails = change.fullDocument;
+        pusher.trigger('messages', 'inserted', 
+            {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received,
+            }
+        );
+    } else {
+       console.log('Error triggering Pusher')
+    }
+
+  });
+});
+
 
 // api routes 
-app.get('/',(req,res)=>res.status(200).send('hello world'))
+app.get("/", (req,res) => res.status(200).send("hello world"));
+
+app.get('/messages/sync', async (req, res) => {
+    try {
+        const data = await Messages.find();
+        res.status(200).send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.post('/messages/new', async (req, res) => {
+    const dbMessage = req.body;
+
+    try {
+        const data = await Messages.create(dbMessage);
+        res.status(201).send(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 // listen
-app.listen(port,()=>console.log(`Listening on localhost:${port}`))
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Chat.css';
 import { Avatar, IconButton } from '@mui/material';
 import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import MicIcon from "@mui/icons-material/Mic";
+import axios from "./axios";
 
-function Chat() {
+function Chat({ messages }) {
+    const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await axios.post("/messages/new", {
+    message: input,
+    name: "DEMO APP",
+    timestamp: "Just Now",
+    received: false,
+    });
+
+    setInput('');
+  }; 
+  
   return (
     <div className='chat'>
       <div className='chat__header'>
@@ -30,29 +46,26 @@ function Chat() {
       </div>
 
       <div className="chat__body">
-        <p className='chat__message'>
-          <span className='chat__name'>Jawad</span>
-          This is a message
+        {messages.map((message) => (
+          <p className={`chat__message ${message.received && "chat__reciever"}`}>
+          <span className='chat__name'>{message.name}</span>
+          {message.message}
           <span className='chat__timestamp'>
-            {new Date().toUTCString()}
+            {message.timestamp}
           </span>
-          </p>
-
-          <p className='chat__message chat__reciever'>
-          <span className='chat__name'>Jawad</span>
-          This is a message
-          <span className='chat__timestamp'>
-            {new Date().toUTCString()}
-          </span>
-          </p>
-      </div>
+          </p> 
+        ))}
+        </div>
 
       <div className='chat__footer'>
         <InsertEmoticonIcon />
         <form>
-          <input placeholder='Type a message'
-            type='text'/>
-          <button type='submit'> Send a message
+          <input 
+           value={input}
+           onChange={(e) => setInput(e.target.value)}
+           placeholder='Type a message'
+           type='text'/>
+          <button onClick={sendMessage} type='submit'> Send a message
           </button>
         </form>
         <MicIcon />
